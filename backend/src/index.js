b5561ff9a2a6d4f083fc1a3563e8e0add58a431e
Rohit Paul <paulrohit85@gmail.com>
2026-03-14 18:47:56 +0530

    feat: add backend and frontend project folders

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+dotenv.config({path:"./.env"})
+const app = express()
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
