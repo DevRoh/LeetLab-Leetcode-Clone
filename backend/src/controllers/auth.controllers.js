96ec4c72b144b964a1154f6827a06bfa4b1840a4
Rohit Paul <paulrohit85@gmail.com>
2026-03-27 01:23:49 +0530

    feat: implement user registration with JWT cookie auth

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { email, password, name } = req.body;
@@ -10,7 +12,50 @@ export const register = async (req, res) => {
         email,
       },
     });
-  } catch (error) {}
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "User already exists", success: false });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: UserRole.USER,
+      },
+    });
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    res.status(201).json({
+      message: "User created Successfully",
+      user: {
+        email: newUser.email,
+        name: newUser.name,
+        role: newUser.role,
+        image: newUser.image,
+      },
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error creating user: ", error);
+    res.status(500).json({
+      error: "Error creating user",
+    });
+  }
 };
 
 export const login = async (req, res) => {};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,22 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import authRoutes from './routes/auth.routes.js'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 
-dotenv.config({path:"./.env"})
-const app = express()
+dotenv.config({ path: "./.env" });
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.get("/",(req,res)=> {
-    res.send("Hello Welcome to Leetlab")
-})
+app.get("/", (req, res) => {
+  res.send("Hello Welcome to Leetlab");
+});
 
-app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/auth", authRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
