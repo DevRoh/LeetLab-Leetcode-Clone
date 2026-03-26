3ed8517b6326f42b1e1174de9a4dfde45d189ce7
Rohit Paul <paulrohit85@gmail.com>
2026-03-27 01:39:03 +0530

    fix: update user schema and load env in prisma db setup

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -1,11 +1,13 @@
 import { PrismaClient } from "../generated/prisma/index.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 const globalForPrisma = globalThis;
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-export const db =
-  globalForPrisma.prisma || new PrismaClient({ adapter });
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
