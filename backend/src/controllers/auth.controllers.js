6c72baff68c5035effda9dc2e637377d7b1869ec
Rohit Paul <paulrohit85@gmail.com>
2026-03-27 01:49:46 +0530

    feat: add login controller and auth input validation

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -5,7 +5,9 @@ import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { email, password, name } = req.body;
-
+  if (!email || !password || !name) {
+    return res.status(401).json({ message: "Please provide all fields" });
+  }
   try {
     const existingUser = await db.user.findUnique({
       where: {
@@ -58,7 +60,55 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(401)
+      .json({ message: "Please provide all the credentials" });
+  }
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!email) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    const isMatched = await bcrypt.compare(password,user.password)
+
+    if(!isMatched) {
+      res.status(401).json({error:"Invalid credentials"})
+    }
+
+    const token  = jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'7d'})
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    res.status(201).json({
+      message: "User logged-in Successfully",
+      user: {
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        image: user.image,
+      },
+      success: true,
+    });
+
+  } catch (error) {
+    console.error("Error logging in user: ", error);
+    res.status(500).json({
+      error: "Error logging in user",
+    });
+  }
+};
 
 export const logout = async (req, res) => {};
 
